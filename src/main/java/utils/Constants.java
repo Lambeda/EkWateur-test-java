01e0d3790f852e75cf4facd5a91caf589c2aa073
cindy <cindylim@free.fr>
2023-05-30 00:39:23 +0200

    add 2 fichiers oubliés

---
+++ src/main/java/utils/Constants.java
@@ -0,0 +1,15 @@
+package main.java.utils;
+
+public final class Constants {
+    private Constants() {
+        // restrict instantiation
+    }
+
+    public static final int CA_DELIMITEUR = 1000000;
+    public static String dateFormat = "dd-MM-yyyy";
+    public static final String pathToClientDB = "src/test/resources/ClientDB";
+    public static final String pathToClientPartDB = "src/test/resources/ClientPartDB";
+    public static final String pathToClientProDB = "src/test/resources/ClientProDB";
+    public static final String pathToConsommationDB = "src/test/resources/ConsommationDB";
+    public static final String pathToTarifDB = "src/test/resources/TarifDB";
+}
